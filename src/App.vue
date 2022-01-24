62a2fd4abe4242be764137e9e8e6addb604dfa0e
PandA <1554296563@qq.com>
2022-01-24 20:49:41 +0800

    label 换一个图标

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
 <template>
-  <div>
+  <div id="app">
     <router-view></router-view>
   </div>
 </template>
 
 <style lang="scss">
-@import "./assets/style/reset";
+@import "./assets/style/helper.scss";
+@import "./assets/style/reset.scss";
+body {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #333;
+  line-height: 1.5;
+  font-family: $font-hei;
+  font-size: 16px;
+}
 </style>
