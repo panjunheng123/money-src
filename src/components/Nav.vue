b4660fba8de5c00d96c5212681826f29f744f386
PandA <1554296563@qq.com>
2022-01-24 15:13:14 +0800

    Money 页面布局

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="nav">
     <router-link to="/money">记账</router-link>
     |
     <router-link to="/labels">标签</router-link>
@@ -19,5 +19,7 @@ export default class Nav extends Vue {
 </script>
 
 <style lang="scss" scoped>
-
+  .nav{
+    border: 1px solid red;
+  }
 </style>
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>Money.vue
+  <div class="nav-wrapper">
+    <div class="content">
+      <p>Money.vue</p>
+    </div>
     <Nav></Nav>
   </div>
 </template>
@@ -15,5 +18,14 @@ export default class Money extends Vue {
 </script>
 
 <style lang="scss" scoped>
-
+  .nav-wrapper{
+    border: 1px solid green;
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+  }
+  .content{
+    overflow: auto;
+    flex-grow: 1;
+  }
 </style>
