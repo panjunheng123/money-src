b86d7a00ecfaa73f42e7a1e6edc4ba98572c2ca9
PandA <1554296563@qq.com>
2022-01-24 17:30:42 +0800

    Nav.vue 添加样式

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,35 +1,47 @@
 <template>
-  <div class="nav">
-    <router-link to="/money">
-      <Icon name="money"></Icon>
+  <nav>
+    <router-link to="/money" class="item" active-class="selected">
+      <Icon name="money"/>
       记账
     </router-link>
-    |
-    <router-link to="/labels">
-      <Icon name="label"></Icon>
+    <router-link to="/labels" class="item" active-class="selected">
+      <Icon name="label"/>
       标签
     </router-link>
-    |
-    <router-link to="/statistics">
-      <Icon name="statistics"></Icon>
+    <router-link to="/statistics" class="item" active-class="selected">
+      <Icon name="statistics"/>
       统计
     </router-link>
-  </div>
+  </nav>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import {Component} from 'vue-property-decorator';
-
-
-@Component
-export default class Nav extends Vue {
-
-}
+export default {
+  name: 'Nav'
+};
 </script>
 
 <style lang="scss" scoped>
-.nav {
-  border: 1px solid red;
+@import "./src/assets/style/helper";
+nav {
+  @extend %outerShadow;
+  display: flex;
+  flex-direction: row;
+  font-size: 12px;
+  > .item {
+    padding: 2px 0;
+    width: 33.33333%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    .icon {
+      width: 32px;
+      height: 32px;
+    }
+  }
+  > .item.selected{
+    color: $color-highlight;
+  }
 }
 </style>
