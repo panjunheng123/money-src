bb0d7543310916e4b24891473f4abe8d451da0ce
PandA <1554296563@qq.com>
2022-01-25 21:10:25 +0800

    收集四个组件的数据到 record

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
     <ul class="types">
-      <li :class="type==='-'&&'selected'"
+      <li :class="value==='-'&&'selected'"
           @click="selectType('-')">
         支出
       </li>
-      <li :class="type==='+'&&'selected'"
+      <li :class="value==='+'&&'selected'"
           @click="selectType('+')">
         收入
       </li>
@@ -14,18 +14,18 @@
 </template>
 
 <script lang="ts">
-import {Component} from 'vue-property-decorator';
+import {Component, Prop} from 'vue-property-decorator';
 import Vue from 'vue';
 
 @Component
 export default class Types extends Vue {
-  type = '-';
+  @Prop() readonly value!: string;
 
   selectType(type: string) {
     if (type !== '-' && type !== '+') {
       throw new Error('type is unknown');
     }
-    this.type = type;
+    this.$emit('update:value',type)
   }
 }
 </script>
