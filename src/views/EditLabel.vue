fff762a764108265b9604f48db4987454832b10c
PandA <1554296563@qq.com>
2022-01-26 15:19:23 +0800

    封装 Button，完成 EditLabel 的样式

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -1,10 +1,16 @@
 <template>
   <Layout>
-    <div>
-      <Icon name="left"></Icon>
-      <span>编辑标签</span>
+    <div class="navBar">
+      <Icon class="leftIcon" name="left"/>
+      <span class="title">编辑标签</span>
+      <span class="rightIcon"></span>
     </div>
-    <FormItem field-name="标签名" placeholder="请输入标签名"></FormItem>
+    <div class="form-wrapper">
+      <FormItem field-name="标签名" placeholder="请输入标签名"/>
+    </div>
+    <div class="button-wrapper">
+      <Button>删除标签</Button>
+    </div>
   </Layout>
 </template>
 
@@ -13,14 +19,14 @@ import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
 import tagListModel from '@/models/tagListModel';
 import FormItem from '@/components/Money/FormItem.vue';
-
+import Button from '@/components/Button.vue';
 @Component({
-  components: {FormItem}
+  components: {Button, FormItem}
 })
 export default class EditLabel extends Vue {
   created() {
     const id = this.$route.params.id;
-    tagListModel.fetch;
+    tagListModel.fetch();
     const tags = tagListModel.data;
     const tag = tags.filter(t => t.id === id)[0];
     if (tag) {
@@ -33,5 +39,37 @@ export default class EditLabel extends Vue {
 </script>
 
 <style lang="scss" scoped>
+.navBar {
+  text-align: center;
+  font-size: 16px;
+  padding: 12px 16px;
+  background: white;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 
+  > .title {
+  }
+
+  > .leftIcon {
+    width: 24px;
+    height: 24px;
+  }
+
+  > .rightIcon {
+    width: 24px;
+    height: 24px;
+  }
+}
+
+.form-wrapper {
+  background: white;
+  margin-top: 8px;
+}
+
+.button-wrapper {
+  text-align: center;
+  padding: 16px;
+  margin-top: 44-16px;
+}
 </style>
