afa062c406bf5c16937c12c3d9c82de6dc7c5efa
PandA <1554296563@qq.com>
2022-02-07 23:29:26 +0800

    Tags.vue 展示icon

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -4,13 +4,20 @@
       <li v-for="tag in tagList" :key="tag.id"
           v-show="tag.type===type"
           :class="{selected:selectedTags.indexOf(tag)>=0}"
-          @click="toggle(tag)">{{ tag.name }}
-          <Icon :name="tag.iconName"></Icon>
+          @click="toggle(tag)">
+        <Icon :name="tag.iconName" class="myIcon"></Icon>
+        <span class="myFont">{{ tag.name }}</span>
+      </li>
+      <li>
+        <router-link to="/labels" class="item" active-class="selected">
+          <Icon class="myIcon" name="设置"/>
+          <span class="myFont">设置</span>
+        </router-link>
       </li>
     </ul>
-    <!--    <div class="new">-->
-    <!--      <button @click="createTag">新增标签</button>-->
-    <!--    </div>-->
+    <!--        <div class="new">-->
+    <!--          <button @click="createTag">新增标签</button>-->
+    <!--        </div>-->
   </div>
 
 </template>
@@ -49,31 +56,44 @@ export default class Tags extends mixins(TagHelper) {
 </script>
 
 <style lang="scss" scoped>
+@import "./src/assets/style/helper";
 .tags {
-  background: white;
+  //background: white;
   font-size: 14px;
   padding: 16px;
   flex-grow: 1;
-  //display: flex;
 
   > .current {
     display: flex;
     flex-wrap: wrap;
 
     > li {
-      $bg: #d9d9d9;
+      //$bg: #d9d9d9;
+      $bg: rgb(236, 236, 236);
       background: $bg;
-      $h: 24px;
+      $h: 80px;
       height: $h;
-      line-height: $h;
+      width: $h;
       border-radius: $h/2;
-      padding: 0 16px;
-      margin-right: 12px;
+      padding: 6px 18px;
+      margin-left: 8px;
       margin-top: 4px;
+      display: flex;
+      flex-direction: column;
 
       &.selected {
-        background: darken($bg, 50%);
-        color: white;
+        background: darken($bg, 35%);
+        color: #fff;
+        //border: 1px solid $color-highlight;
+      }
+
+      .myIcon {
+        color: #333;
+        font-size: 46px;
+      }
+
+      .myFont {
+        margin-left: 8px;
       }
     }
   }
--- src/store/index.ts
+++ src/store/index.ts
@@ -74,13 +74,10 @@ const store = new Vuex.Store({
       if (!state.tagList || state.tagList.length === 0) {
         store.commit('createTag', {name: '餐饮', type: '-', iconName: '餐饮'});
         store.commit('createTag', {name: '购物', type: '-', iconName: '购物'});
-        store.commit('createTag', {name: '娱乐', type: '-', iconName: '吃豆人'});
-        store.commit('createTag', {name: '交通', type: '-', iconName: '交通'});
+        store.commit('createTag', {name: '娱乐', type: '-', iconName: '娱乐'});
+        store.commit('createTag', {name: '交通', type: '-', iconName: '汽车'});
         store.commit('createTag', {name: '零食', type: '-', iconName: '零食'});
         store.commit('createTag', {name: '水果', type: '-', iconName: '樱桃'});
-        store.commit('createTag', {name: '购物', type: '-', iconName: '购物'});
-        store.commit('createTag', {name: '娱乐', type: '-', iconName: '吃豆人'});
-        store.commit('createTag', {name: '交通', type: '-', iconName: '交通'});
         store.commit('createTag', {name: '其它', type: '+', iconName: '钱包'});
         store.commit('createTag', {name: '工资', type: '+', iconName: '工资'});
         store.commit('createTag', {name: '兼职', type: '+', iconName: '兼职'});
