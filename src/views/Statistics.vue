b910008ee0219ad9d77910270d5fa6add037e25c
PandA <1554296563@qq.com>
2022-02-08 14:05:39 +0800

    finish Statistics.vue

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -2,12 +2,26 @@
   <Layout>
     <Tabs :data-source="recordTypeList" :value.sync="type"></Tabs>
     <Tabs class-prefix="date" :data-source="dateTypeList" :value.sync="date"></Tabs>
-    <ol v-if="groupedList.length>0">
-      <li v-for="(group,index) in groupedList" :key="index">
-        <h3 class="title">{{ beautify(group.title) }} <span>￥{{ group.total }}</span></h3>
+    <ol v-if="groupedDayList.length>0&&date==='day'">
+      <li v-for="(group,index) in groupedDayList" :key="index">
+        <h3 class="title">{{ dayBeautify(group.title) }} <span>￥{{ group.total }}</span></h3>
         <ol>
           <li v-for="item in group.items" :key="item.id" class="record">
-            <span>{{ tagString(item.tags) }}</span>
+            <Icon :name="tagIconNameString(item.tags)"></Icon>
+            <span>{{ tagNameString(item.tags) }}</span>
+            <span class="notes" :style="{marginRight:'auto'}">{{ item.notes }}</span>
+            <span>￥{{ item.amount }}</span>
+          </li>
+        </ol>
+      </li>
+    </ol>
+    <ol v-else-if="groupedMonthList.length>0&&date==='month'">
+      <li v-for="(group,index) in groupedMonthList" :key="index">
+        <h3 class="title">{{ monthBeautify(group.title) }} <span>￥{{ group.total }}</span></h3>
+        <ol>
+          <li v-for="item in group.items" :key="item.id" class="record">
+            <Icon :name="tagIconNameString(item.tags)"></Icon>
+            <span>{{ tagNameString(item.tags) }}</span>
             <span class="notes" :style="{marginRight:'auto'}">{{ item.notes }}</span>
             <span>￥{{ item.amount }}</span>
           </li>
@@ -45,6 +59,12 @@
 .record {
   background: rgb(236, 236, 236);
   @extend %item;
+
+  > svg {
+    width: 24px;
+    height: 24px;
+    margin-right: 12px;
+  }
 }
 
 .notes {
@@ -74,11 +94,16 @@ export default class Statistics extends Vue {
   date = 'day';
 
   // eslint-disable-next-line no-undef
-  tagString(tags: Tag[]) {
+  tagNameString(tags: Tag[]) {
     return tags.length === 0 ? '无' : tags.map(t => t.name).join('，');
   }
 
-  beautify(string: string) {
+  // eslint-disable-next-line no-undef
+  tagIconNameString(tags: Tag[]) {
+    return tags.length === 0 ? '无' : tags.map(t => t.iconName).join('，');
+  }
+
+  dayBeautify(string: string) {
     const day = dayjs(string);
     const now = dayjs();
     if (day.isSame(now, 'day')) {
@@ -92,11 +117,23 @@ export default class Statistics extends Vue {
     }
   }
 
+  monthBeautify(string: string) {
+    const day = dayjs(string);
+    const now = dayjs();
+    if (day.isSame(now, 'month')) {
+      return '本月';
+    } else if (day.isSame((now), 'year')) {
+      return day.format('M月');
+    } else {
+      return day.format('YYYY年M月');
+    }
+  }
+
   get recordList() {
     return this.$store.state.recordList;
   }
 
-  get groupedList() {
+  get groupedDayList() {
     const {recordList} = this;
 
     const newList = clone(recordList).filter((r: RecordItem) => r.type === this.type).sort((a: RecordItem, b: RecordItem) => dayjs(b.createdAt).valueOf() - dayjs(a.createdAt).valueOf());
@@ -107,6 +144,28 @@ export default class Statistics extends Vue {
       const current = newList[i];
       const last = result[result.length - 1];
       if (dayjs(last.title).isSame(dayjs(current.createdAt), 'day')) {
+        last.items.push(current);
+      } else {
+        result.push({title: dayjs(current.createdAt).format('YYYY-MM-DD'), items: [current]});
+      }
+    }
+    result.map(group => {
+      group.total = group.items.reduce((sum, item) => sum + item.amount, 0);
+    });
+    return result;
+  }
+
+  get groupedMonthList() {
+    const {recordList} = this;
+
+    const newList = clone(recordList).filter((r: RecordItem) => r.type === this.type).sort((a: RecordItem, b: RecordItem) => dayjs(b.createdAt).valueOf() - dayjs(a.createdAt).valueOf());
+    if (newList.length === 0) {return [] as Result;}
+    type Result = { title: string, total?: number, items: RecordItem[] }[]
+    const result: Result = [{title: dayjs(newList[0].createdAt).format('YYYY-MM-DD'), items: [newList[0]]}];
+    for (let i = 1; i < newList.length; i++) {
+      const current = newList[i];
+      const last = result[result.length - 1];
+      if (dayjs(last.title).isSame(dayjs(current.createdAt), 'month')) {
         last.items.push(current);
       } else {
         result.push({title: dayjs(current.createdAt).format('YYYY-MM-DD'), items: [current]});
